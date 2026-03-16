f97b553f720e2be9f215cecf101b0f7d9ddabafd
Edward-Andrei-2005 <edwardandrei192@gmail.com>
2026-03-16 10:50:26 +0100

    empezado contrato de aeropuerto, +mint

--- contract.sol
+++ contract.sol
@@ -5,19 +5,47 @@ import "./erc20.sol";
 
 contract AirportContract {
     string private _name;
-    string private _symbol;
-
-    ERC20Simple public _token;
-
+    ERC20Simple private _token;
     uint256 private _totalSupply;
-    
-    mapping(address => uint256) private _balances;
+    address payable public owner;
+
+    struct Passenger {
+        address id;
+        uint256 balance;
+        string name;
+    }
+
+    mapping(address => Passenger) private listPassengers;
 
     constructor() {
-        _token = new ERC20Simple("Mi token", "token", 5, 0);
+        _name = "Airport";
+        owner = payable(msg.sender);
+        _token = new ERC20Simple("Vuelitos", "Vlt", 5, 0);
     }
 
     function getToken() external view returns (ERC20Simple) {
         return _token;
     }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    function register(address id_, string memory name_) internal onlyOwner  {
+        require(listPassengers[id_].id == address(0), "Passenger is already registered");
+
+        Passenger memory passenger = Passenger({
+            id: id_,
+            balance: 0,
+            name: name_
+        });
+
+        listPassengers[id_] = passenger;
+    }
+
+    function assignToken(address id, uint256 miles) internal onlyOwner {
+        _token.mint(id, miles);
+        listPassengers[id].balance += miles;
+    } 
 }
